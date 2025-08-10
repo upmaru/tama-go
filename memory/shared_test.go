6fa8faeeb2a6705fdb70eac00140e60daf336f90
Zack Siri <me@zacksiri.com>
2025-08-10 13:05:06 +0700

    fix linting issues

--- memory/shared_test.go
+++ memory/shared_test.go
@@ -3,22 +3,9 @@ package memory_test
 import (
 	"net/http"
 	"net/http/httptest"
-	"time"
-
-	tama "github.com/upmaru/tama-go"
 )
 
 // CreateMockServer creates a test HTTP server with the given handler.
 func CreateMockServer(handler http.HandlerFunc) *httptest.Server {
 	return httptest.NewServer(handler)
-}
-
-// createTestClient is a helper to create a test client with a given base URL.
-func createTestClient(baseURL string) *tama.Client {
-	config := tama.Config{
-		BaseURL: baseURL,
-		APIKey:  "test-key",
-		Timeout: 10 * time.Second,
-	}
-	return tama.NewClient(config)
 }
